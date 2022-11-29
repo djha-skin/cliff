eb7b42af824eca3fcf9567c648d5cb9692fef03e
Daniel Jay Haskin <djhaskin987@gmail.com>
2022-11-29 09:51:54 -0700

    Update tmux

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 tmux new-window
-tmux send-keys -l -t @0 "./lighttpd-environment/lighttpd.exp"
+tmux send-keys -l -t @0 "docker-compose up"
 tmux send-keys -l -t @1 "./tools/repl.sh"
 tmux split-window -h
 #tmux bind-key k send-keys -t :1.0 -l "i(rove:run-test *)"

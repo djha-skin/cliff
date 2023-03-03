52cf44b7570944fc574627d0053d44b272d77676
Skin <me@djha.skin>
2023-03-02 19:43:00 -0700

    Handlers for repl loading

--- tools/preload.lisp
+++ tools/preload.lisp
@@ -4,8 +4,17 @@
 ;    (merge-pathnames #P".qlot/" (uiop/os:getcwd))))
 ;
 ;(setf (uiop/os:getenv "QUICKLISP_HOME") cl-i-quicklisp)
+;(setf (uiop/os:getenv "CL_SOURCE_REGISTRY") (uiop:native-namestring (uiop/os:getcwd)))
 ;
 ;(setf ql:*quicklisp-home* cl-i-quicklisp)
 (let* ((name "cl-i"))
+  (handler-bind ((uiop/lisp-build:compile-file-error (lambda (condt) (declare (ignore condt)) (invoke-restart (find-restart 'accept)))))
+    (handler-bind ((asdf/find-component:missing-component (lambda (condt)
+                                                            (declare (ignore condt))
+                                                            (invoke-restart (find-restart 'continue)))))
+      (handler-bind ((error (lambda (condt)
+                              (declare (ignore condt))
+                              (invoke-restart (find-restart 'continue)))))
   (asdf:load-system name)
-  (asdf:load-system (format nil "~A/tests" name)))
+  (asdf:load-system (format nil "~A/tests" name))
+  ))))

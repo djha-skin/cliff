e5c053be7e6b1acdb2fc7f0399c9d05f7312b765
Skin <me@djha.skin>
2023-03-04 07:49:58 -0700

    For tests

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#tmux new-window
-#tmux send-keys -l -t @0 "docker-compose up"
+tmux new-window
+tmux send-keys -l -t @0 "docker-compose up"
 tmux split-window -h
 tmux send-keys -l -t :0.0 "./tools/repl.sh"
-#tmux bind-key k send-keys -t :0.0 -l "i(rove:run-test *)"
+tmux bind-key k send-keys -t :0.0 -l "i(rove:run-test *)"
 tmux bind-key k send-keys -t :0.0 -l "(rove:run-test *)"

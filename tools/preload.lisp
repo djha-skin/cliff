2ee95e002022e39f62d729f5c91c4142e777583f
Skin <me@djha.skin>
2023-03-11 18:47:06 -0700

    Revert "Here we are, CLPM, hopefully"
    This reverts commit 9187c106fabfce590979ed0a1df7406738265cb3.

--- tools/preload.lisp
+++ tools/preload.lisp
@@ -1,3 +1,12 @@
+;(asdf:load-system "qlot")
+;(defvar cl-i-quicklisp
+;  (uiop:native-namestring
+;    (merge-pathnames #P".qlot/" (uiop/os:getcwd))))
+;
+;(setf (uiop/os:getenv "QUICKLISP_HOME") cl-i-quicklisp)
+;(setf (uiop/os:getenv "CL_SOURCE_REGISTRY") (uiop:native-namestring (uiop/os:getcwd)))
+;
+;(setf ql:*quicklisp-home* cl-i-quicklisp)
 (let* ((name "cl-i"))
   (handler-bind ((uiop/lisp-build:compile-file-error (lambda (condt) (declare (ignore condt)) (invoke-restart (find-restart 'accept)))))
     (handler-bind ((asdf/find-component:missing-component (lambda (condt)
@@ -7,4 +16,5 @@
                               (declare (ignore condt))
                               (invoke-restart (find-restart 'continue)))))
   (asdf:load-system name)
-  (asdf:load-system (format nil "~A-tests" name))))))
+  (asdf:load-system (format nil "~A/tests" name))
+  ))))
--- tools/repl.sh
+++ tools/repl.sh
@@ -2,15 +2,6 @@
 export RLWRAP_HOME=${PWD}/.repl
 export C_INCLUDE_PATH=/usr/include
 export LIBRARY_PATH=/usr/lib64:/usr/lib
-export CL_REGISTRY
-
-
-#export CL_SOURCE_REGISTRY="C:/Users/bhw/quicklisp/dists/quicklisp/software/qlot-20220331/git/;C:/Users/bhw/Code/lisp/cl-i;C:/Users/bhw/Code/lisp;C:/Users/bhw/Code/lisp/asdf"
-
-#set QUICKLISP_HOME="C:/Users/bhw/Code/lisp/cl-i/.qlot"
-
-clpm bundle exec -- "C:\Program Files (x86)\ecl\ecl.exe" -load tools/preload.lisp
-
 #rlwrap \
 #    --ansi-colour-aware \
 #    --no-children \
@@ -26,5 +17,5 @@ clpm bundle exec -- "C:\Program Files (x86)\ecl\ecl.exe" -load tools/preload.lis
 #    --prompt-colour=red \
 #    --quote-characters "\"" \
 #    --remember \
-#qlot exec ros run -S . -- --load tools/preload.lisp
+    qlot exec ros run -S . -- -load tools/preload.lisp
 #sbcl --load tools/preload.lisp
